3865e0206eb09e3f6df4ce5253e210f28053f67d
Adrian Baran <adrianbaranweb@gmail.com>
2020-11-30 16:38:29 +0100

    Add drawing on canvas

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -3,6 +3,11 @@ import {Common, VISIBLE_SCREEN} from './Common.esm.js';
 import {gameLevels} from './gameLevels.esm.js';
 import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js'
 
+const gameState = {
+    pointsToWin: 7000,
+    getPlayerPoints: () => 1000,
+    getLeftMovement: () => 10,
+}
 
 class Game extends Common{
     constructor(){
@@ -17,7 +22,7 @@ class Game extends Common{
     }
 
     animate(){
-        canvas.drawGameOnCanvas();
+        canvas.drawGameOnCanvas(gameState);
         this.animationFrame = window.requestAnimationFrame(() => this.animate());
     }
 }
